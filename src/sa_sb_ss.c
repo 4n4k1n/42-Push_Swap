2f98578de5963ffece80373e69395ebf0106571b
4n4k1n <anakin@pregitzer.de>
2025-05-15 16:42:35 +0200

    functions to swap elements

---
+++ src/sa_sb_ss.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sa_sb_ss.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 07:58:11 by apregitz          #+#    #+#             */
+/*   Updated: 2025/05/15 16:38:48 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	sa(void)
+{
+	t_stacks	*data;
+
+	data = get_stacks();
+	data->stack_a.tail->num ^= data->stack_a.tail->prev->num;
+	data->stack_a.tail->prev->num ^= data->stack_a.tail->num;
+	data->stack_a.tail->num ^= data->stack_a.tail->prev->num;
+}
+
+void	sb(void)
+{
+	t_stacks	*data;
+
+	data = get_stacks();
+	data->stack_b.tail->num ^= data->stack_b.tail->prev->num;
+	data->stack_b.tail->prev->num ^= data->stack_b.tail->num;
+	data->stack_b.tail->num ^= data->stack_b.tail->prev->num;
+}
+
+void	ss(void)
+{
+	sa();
+	sb();
+	}
