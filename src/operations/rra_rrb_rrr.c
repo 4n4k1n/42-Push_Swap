8112bd502018e0912213381b0231517f60d5f275
4n4k1n <anakin@pregitzer.de>
2025-05-25 15:26:29 +0200

    operations to reverse rotate the stack by one element

---
+++ src/operations/rra_rrb_rrr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rra_rrb_rrr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 11:52:08 by apregitz          #+#    #+#             */
+/*   Updated: 2025/05/25 15:25:08 by hntest2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rra(void)
+{
+	t_data	*data;
+
+	data = get_data();
+	data->stack_a.head->prev = data->stack_a.tail;
+	data->stack_a.tail->next = data->stack_a.head;
+	data->stack_a.head = data->stack_a.head->prev;
+	data->stack_a.tail = data->stack_a.tail->prev;
+	data->stack_a.head->prev = NULL;
+	data->stack_a.tail->next = NULL;
+}
+
+void	rra(void)
+{
+	t_data	*data;
+
+	data = get_data();
+	data->stack_b.head->prev = data->stack_b.tail;
+	data->stack_b.tail->next = data->stack_b.head;
+	data->stack_b.head = data->stack_b.head->prev;
+	data->stack_b.tail = data->stack_b.tail->prev;
+	data->stack_b.head->prev = NULL;
+	data->stack_b.tail->next = NULL;
+}
+
+void    rrr(void)
+{
+    rra();
+    rrb();
+}
