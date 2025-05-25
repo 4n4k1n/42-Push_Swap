8470bc32bdf824b507121cdc196b60bce1d3dc1c
4n4k1n <anakin@pregitzer.de>
2025-05-25 16:23:26 +0200

    main function for push swap

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 09:56:19 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/14 16:39:59 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/25 12:33:32 by hntest2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/push_swap.h"
+#include "push_swap.h"
 
 int	main(int ac, char **av)
 {
