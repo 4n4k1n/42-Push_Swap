2156151a54d635a37af31cd05c9724a60c4fce4a
4n4k1n <anakin@pregitzer.de>
2025-05-25 15:21:02 +0200

    operations to swap the first 2 elements on one stack

---
+++ src/operations/sa_sb_ss.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sa_sb_ss.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 07:58:11 by apregitz          #+#    #+#             */
+/*   Updated: 2025/05/25 15:20:07 by hntest2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(void)
+{
+	t_data	*data;
+
+	data = get_stacks();
+	if (!data->stack_a.head->next)
+		return ;
+	data->stack_a.head->num ^= data->stack_a.head->next->num;
+	data->stack_a.head->next->num ^= data->stack_a.head->num;
+	data->stack_a.head->num ^= data->stack_a.head->next->num;
+}
+
+void	sb(void)
+{
+	t_data	*data;
+
+	data = get_stacks();
+	if (!data->stack_b.head->next)
+		return ;
+	data->stack_b.head->num ^= data->stack_b.head->next->num;
+	data->stack_b.head->next->num ^= data->stack_b.head->num;
+	data->stack_b.head->num ^= data->stack_b.head->next->num;
+}
+
+void	ss(void)
+{
+	sa();
+	sb();
+}
