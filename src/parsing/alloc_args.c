f5f42b3adbc025e8951d40d425c480a0f71d8c50
4n4k1n <anakin@pregitzer.de>
2025-05-26 13:41:43 +0200

    fixed heap buffer overflow

--- src/parsing/alloc_args.c
+++ src/parsing/alloc_args.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:15:25 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/26 10:15:27 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/26 13:37:43 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ static t_llist	*split_arg(char *str, t_llist *llist)
 	i = -1;
 	while (arr[++i])
 	{
-		if (!ft_atoll(arr[i], llist))
-			ft_exit(1);
+		llist->num = ft_atoi(arr[i]);
 		llist = llist->next;
 	}
 	ft_free_split(arr);
@@ -34,13 +33,15 @@ static t_llist	*split_arg(char *str, t_llist *llist)
 void	alloc_args(t_data *data)
 {
 	int		i;
+	int		index;
 	t_llist	*temp;
 
-	data->stack_a.head = ft_malloc(data->words_count * sizeof(long long));
+	data->stack_a.head = create_llist(data->words_count);
 	if (!data->stack_a.head)
 		ft_exit(1);
-	i = 0;
 	temp = data->stack_a.head;
+	index = 0;
+	i = 0;
 	while (++i < data->ac)
 		temp = split_arg(data->av[i], temp);
 }
