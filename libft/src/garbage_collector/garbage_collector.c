142a1b91f61a1a08278a5e24851ca885edf60212
4n4k1n <anakin@pregitzer.de>
2025-05-26 07:46:06 +0200

    moved the get data function on top of the file and called it in the ft_malloc function

--- libft/src/garbage_collector/garbage_collector.c
+++ libft/src/garbage_collector/garbage_collector.c
@@ -6,12 +6,18 @@
 /*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 13:05:17 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/25 14:22:54 by hntest2          ###   ########.fr       */
+/*   Updated: 2025/05/26 07:08:03 by hntest2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gargage_collector.h"
 
+static t_gcll_data	*get_gcll_data(void)
+{
+	static t_gcll_data data;
+
+	return (&data);
+}
 void	*ft_add_to_gc(void *ptr)
 {
 	t_gcll_data	*data;
@@ -27,12 +33,6 @@ void	*ft_add_to_gc(void *ptr)
 	return (ptr);
 }
 
-static t_gcll_data	*get_gcll_data(void)
-{
-	static t_gcll_data data;
-
-	return (&data);
-}
 
 void ft_free(void)
 {
@@ -55,6 +55,7 @@ void	*ft_malloc(size_t size)
 	t_gcll_data	*data;
 	t_gcll		*node;
 
+	data = get_gcll_data();
 	node = malloc(sizeof(t_gcll));
 	if (!node)
 		return (NULL);
