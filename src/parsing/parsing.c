b20d4d27e932ff89b4b3021b09e76403e16ca1d9
4n4k1n <anakin@pregitzer.de>
2025-05-26 05:45:01 +0200

    parsing function

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 13:02:15 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/25 13:13:07 by hntest2          ###   ########.fr       */
+/*   Updated: 2025/05/26 05:35:07 by hntest2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 int parsing(int ac, char **av)
 {
-    if (!valid_input(ac, av))
-        return (0);
-    
+    t_data  *data;
+
+    data = get_data();
+    data->ac = ac;
+    data->av = av;
+    if (!valid_input(data))
+        ft_exit(1);
+    data->words_count = count_numbers(data);
+    if (data->words_count < 1)
+        ft_exit(1);
+    alloc_args(data);
+    return (1);
 }
