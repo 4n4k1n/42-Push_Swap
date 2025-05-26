758874d12c67a2c4cece1b0a62e65e258a46198e
4n4k1n <anakin@pregitzer.de>
2025-05-26 08:09:09 +0200

    fixed typo

--- src/logic/logic.c
+++ src/logic/logic.c
@@ -6,7 +6,7 @@
 /*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 05:51:11 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/26 07:35:17 by hntest2          ###   ########.fr       */
+/*   Updated: 2025/05/26 08:07:41 by hntest2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 int push_to_stack_b(t_data *data)
 {
-	int	count;
+	size_t	count;
 
 	count = 0;
 	data->range = ft_sqrt(data->stack_a.size) * 1.6;
 	while (data->stack_a.size)
 	{
-		if (data->stack_a.head <= count)
+		if (data->stack_a.size <= count)
 		{
 			pb();
 			rb();
 			count++;
 		}
-		else if (data->stack_a.head < count + data->range)
+		else if (data->stack_a.size < count + data->range)
 		{
 			pb();
 			count++;
@@ -34,12 +34,10 @@ int push_to_stack_b(t_data *data)
 		else
 			ra();
 	}
+	return (1);
 }
 
-int push_to_stack_a(t_data *data)
-{
-    
-}
+int	push_to_stack_a(t_data *data);
 
 void	logic(void)
 {
