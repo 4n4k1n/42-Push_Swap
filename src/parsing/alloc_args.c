2d321f62428f9848ee34b59c549364dc185e238b
4n4k1n <anakin@pregitzer.de>
2025-05-26 07:40:27 +0200

    fixed logic error

--- src/parsing/alloc_args.c
+++ src/parsing/alloc_args.c
@@ -6,28 +6,29 @@
 /*   By: hntest2 <hntest2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 13:52:46 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/26 05:35:02 by hntest2          ###   ########.fr       */
+/*   Updated: 2025/05/26 06:58:15 by hntest2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	split_arg(t_data *data, t_llist	**llist)
+static t_llist	*split_arg(char *str, t_llist *llist)
 {
-	int i;
-	char **arr;
+	int		i;
+	char	**arr;
 	
-	arr = ft_split(arr, ' ');
+	arr = ft_split(str, ' ');
 	if (!arr)
 		ft_exit(1);
 	i = -1;
 	while (arr[++i])
 	{
-		if (!ft_atoll(arr[i], *llist))
+		if (!ft_atoll(arr[i], llist))
 			ft_exit(1);
-		llist = (*llist)->next;
+		llist = llist->next;
 	}
 	ft_free_split(arr);
+	return (llist);
 }
 
 void	alloc_args(t_data *data)
@@ -41,5 +42,5 @@ void	alloc_args(t_data *data)
 	i = 0;
 	temp = data->stack_a.head;
 	while (++i < data->ac)
-		split_arg(data, &temp);
+		temp = split_arg(data->av[i], temp);
 }
