e939f9c02f5cd1e4f981d554e3b6453dd7e2c8eb
4n4k1n <anakin@pregitzer.de>
2025-05-27 13:16:55 +0200

    added the index llist function to the parsing function and set the llist size variable in the stack a to the number amount

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:15:43 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/26 10:15:44 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/27 13:14:54 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ int	parsing(int ac, char **av)
 	if (!valid_input(data))
 		ft_exit(1);
 	data->words_count = count_numbers(data);
+	data->stack_a.size = data->words_count;
 	if (data->words_count < 1)
 		ft_exit(1);
 	alloc_args(data);
 	is_unique(data);
+	// print_llist(data->stack_a.head);
+	// printf("\n\n\n\n\n");
+	index_llist(data);
 	return (1);
 }
