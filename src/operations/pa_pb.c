34ec2053a9bd10dda64fa98ff687dfa30a742922
4n4k1n <anakin@pregitzer.de>
2025-05-28 10:16:31 +0200

    added protection for the push functions and added operation prints

--- src/operations/pa_pb.c
+++ src/operations/pa_pb.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 10:09:31 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/27 10:26:01 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/28 10:02:41 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,51 @@
 void	pa(void)
 {
 	t_data	*data;
+	t_llist	*node;
 
 	data = get_data();
-	data->stack_a.head->prev = data->stack_b.head;
+	if (!data->stack_b.head)
+		return;
+	node = data->stack_b.head;
 	data->stack_b.head = data->stack_b.head->next;
-	data->stack_a.head->prev->next = data->stack_a.head;
-	data->stack_a.head = data->stack_a.head->prev;
-	data->stack_a.head->prev = NULL;
-	data->stack_b.head->prev = NULL;
+	if (data->stack_b.head)
+		data->stack_b.head->prev = NULL;
+	else
+		data->stack_b.tail = NULL;
+	node->next = data->stack_a.head;
+	node->prev = NULL;
+	if (data->stack_a.head)
+		data->stack_a.head->prev = node;
+	else
+		data->stack_a.tail = node;
+	data->stack_a.head = node;
 	data->stack_a.size++;
 	data->stack_b.size--;
+	ft_printf("pa\n");
 }
 
 void	pb(void)
 {
 	t_data	*data;
+	t_llist	*node;
 
 	data = get_data();
-	data->stack_b.head->prev = data->stack_a.head;
+	if (!data->stack_a.head)
+		return;
+	node = data->stack_a.head;
 	data->stack_a.head = data->stack_a.head->next;
-	data->stack_b.head->prev->next = data->stack_b.head;
-	data->stack_b.head = data->stack_b.head->prev;
-	data->stack_b.head->prev = NULL;
-	data->stack_a.head->prev = NULL;
+	if (data->stack_a.head)
+		data->stack_a.head->prev = NULL;
+	else
+		data->stack_a.tail = NULL;
+	node->next = data->stack_b.head;
+	node->prev = NULL;
+	if (data->stack_b.head)
+		data->stack_b.head->prev = node;
+	else
+		data->stack_b.tail = node;
+	data->stack_b.head = node;
 	data->stack_a.size--;
 	data->stack_b.size++;
+	ft_printf("pb\n");
 }
