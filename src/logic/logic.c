85cb6bfae46c883d50df4cd64375e08b5cb00e83
4n4k1n <anakin@pregitzer.de>
2025-05-28 07:23:56 +0200

    fixed logic error in the if check

--- src/logic/logic.c
+++ src/logic/logic.c
@@ -6,27 +6,28 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 05:51:11 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/26 10:14:16 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/27 18:01:11 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <math.h>
 
 static int push_to_stack_b(t_data *data)
 {
-	size_t	count;
+	int	count;
 
 	count = 0;
-	data->range = ft_sqrt(data->stack_a.size) * 1.6;
+	data->range = sqrt(data->stack_a.size) * 1.6;
 	while (data->stack_a.size)
 	{
-		if (data->stack_a.size <= count)
+		if (data->stack_a.head->num <= count)
 		{
 			pb();
 			rb();
 			count++;
 		}
-		else if (data->stack_a.size < count + data->range)
+		else if (data->stack_a.head->num < count + data->range)
 		{
 			pb();
 			count++;
