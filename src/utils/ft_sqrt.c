62d8e909e369089ab06514540809bfbd5dc2b560
4n4k1n <anakin@pregitzer.de>
2025-05-28 10:15:15 +0200

    simplified the qrt function

--- src/utils/ft_sqrt.c
+++ src/utils/ft_sqrt.c
@@ -6,37 +6,35 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:16:25 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/27 11:44:38 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/27 20:44:15 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_iterative_power(int nb, int power)
-{
-	int	result;
+// static int	ft_iterative_power(int nb, int power)
+// {
+// 	int	result;
 
-	if (power < 0)
-		return (0);
-	else if (power == 0)
-		return (1);
-	result = nb;
-	while (power > 1)
-	{
-		result = result * nb;
-		power--;
-	}
-	return (result);
-}
+// 	if (power < 0)
+// 		return (0);
+// 	else if (power == 0)
+// 		return (1);
+// 	result = nb;
+// 	while (power > 1)
+// 	{
+// 		result = result * nb;
+// 		power--;
+// 	}
+// 	return (result);
+// }
 
 int	ft_sqrt(int nb)
 {
-	int	check;
+	int	i;
 
-	check = 1;
-	while (ft_iterative_power(check, 2) < nb)
-		check++;
-	// if (ft_iterative_power(check, 2) == nb)
-	// 	return (check);
-	return (check);
+	i = 0;
+	while (i * i < nb)
+		i++;
+	return (i);
 }
