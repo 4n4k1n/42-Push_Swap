b2a1cf1b3db5126780fa45f59e317151ec1df917
4n4k1n <anakin@pregitzer.de>
2025-05-28 10:10:46 +0200

    added debug prints

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:16:38 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/27 17:49:43 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/28 10:02:16 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 void	print_llist(t_llist *temp)
 {
+	int	i;
+
+	i = 0;
 	while (temp)
 	{
-		printf("%d\n", temp->num);
+		i = -1;
+		while (++i <= temp->num)
+			write(1, "-", 1);
+		write(1, "\n", 1);
 		temp = temp->next;
 	}
 }
@@ -26,11 +32,14 @@ int	main(int ac, char **av)
 	if (!parsing(ac, av))
 		ft_exit(1);
 	t_data	*data = get_data();
-	t_llist	*temp = data->stack_a.head;
-	print_llist(temp);
-	printf("\n\n\n");
+	// t_llist	*temp = data->stack_a.head;
+	// print_llist(temp);
+	// printf("\n\n\n");
 	logic();
-	temp = data->stack_a.head;
-	print_llist(temp);
+	// printf("\n\n\n");
+	// temp = data->stack_a.head;
+	// print_llist(temp);
+	printf("\n\nOperations: %zu\n\n", data->operations);
+	ft_free();
 	return (0);
 }
