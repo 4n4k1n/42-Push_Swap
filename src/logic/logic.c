157b60c8db573be051ca6910e488a2aac281b9fd
4n4k1n <anakin@pregitzer.de>
2025-05-28 10:18:16 +0200

    created function to get the index of the highest value of the linked list. and created the second part of the k sort algo

--- src/logic/logic.c
+++ src/logic/logic.c
@@ -6,19 +6,42 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 05:51:11 by hntest2           #+#    #+#             */
-/*   Updated: 2025/05/27 18:01:11 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/28 09:47:12 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <math.h>
 
+static size_t get_highest_number(t_data *data, int	*highest)
+{
+	t_llist	*temp;
+	size_t	i;
+	size_t	index;
+
+	index = 0;
+	i = 0;
+	temp = data->stack_b.head;
+	*highest = temp->num;
+	while (temp->next)
+	{
+		if (*highest < temp->next->num)
+		{
+			index = i;
+			*highest = temp->next->num;
+		}
+		temp = temp->next;
+		i++;
+	}
+	return (index);
+}	
+
 static int push_to_stack_b(t_data *data)
 {
 	int	count;
 
 	count = 0;
-	data->range = sqrt(data->stack_a.size) * 1.6;
+	data->range = ft_sqrt(data->stack_a.size) * 1.6;
 	while (data->stack_a.size)
 	{
 		if (data->stack_a.head->num <= count)
@@ -27,7 +50,7 @@ static int push_to_stack_b(t_data *data)
 			rb();
 			count++;
 		}
-		else if (data->stack_a.head->num < count + data->range)
+		else if (data->stack_a.head->num <= count + data->range)
 		{
 			pb();
 			count++;
@@ -38,10 +61,37 @@ static int push_to_stack_b(t_data *data)
 	return (1);
 }
 
+static void push_to_stack_a(t_data *data)
+{
+	int		highest_num;
+	size_t	index;	
+	
+	while (data->stack_b.size > 0)
+	{
+		index = get_highest_number(data, &highest_num); 
+		if (index < data->stack_b.size / 2)
+		{
+			while (data->stack_b.head->num != highest_num)
+				rb();
+			pa();
+		}
+		else
+		{
+			while (data->stack_b.head->num != highest_num)
+				rrb();
+			pa();
+		}
+	}
+}
+
 void	logic(void)
 {
 	t_data	*data;
 
 	data = get_data();
 	push_to_stack_b(data);
+	// print_llist(data->stack_b.head);
+	// printf("\n\n\n");
+	push_to_stack_a(data);
+	// printf("test test\n");
 }
