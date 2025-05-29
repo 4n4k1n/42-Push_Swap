ebaba0013bb467cb6aedac48e7e5200207eff10b
4n4k1n <anakin@pregitzer.de>
2025-05-29 11:34:13 +0200

    added check if the stack is sorted allready

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:16:38 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/28 10:02:16 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/29 11:28:10 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,12 @@ void	print_llist(t_llist *temp)
 }
 
 int	main(int ac, char **av)
-{ 
+{
 	if (!parsing(ac, av))
 		ft_exit(1);
-	t_data	*data = get_data();
-	// t_llist	*temp = data->stack_a.head;
-	// print_llist(temp);
-	// printf("\n\n\n");
+	if (is_sorted())
+		return (0);
 	logic();
-	// printf("\n\n\n");
-	// temp = data->stack_a.head;
-	// print_llist(temp);
-	printf("\n\nOperations: %zu\n\n", data->operations);
 	ft_free();
 	return (0);
 }
